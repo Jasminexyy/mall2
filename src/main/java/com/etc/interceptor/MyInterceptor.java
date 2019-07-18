3138e31b98e2fd169b5fcd4693120a2481b71ecc
xyy <1907184421@qq.com>
2019-07-19 01:40:34 +0800

    身份拦截

--- src/main/java/com/etc/interceptor/MyInterceptor.java
+++ src/main/java/com/etc/interceptor/MyInterceptor.java
@@ -6,9 +6,20 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyInterceptor implements HandlerInterceptor {
-
+	private static List<String> admins = new ArrayList<>();
+	static {
+		admins.add("admin");
+		admins.add("removegood");
+		admins.add("addgoods");
+		admins.add("doadd");
+		admins.add("editgoods");
+		admins.add("doedit");
+		admins.add("getdata");
+	}
 	@Override
 	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
 			throws Exception {
@@ -27,13 +38,33 @@ public class MyInterceptor implements HandlerInterceptor {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object arg2) throws Exception {
 		HttpSession session = request.getSession();
 		String path = request.getServletPath();
-		if(session.getAttribute("uid")!=null||path.indexOf("login")!=-1)
+		String subpath = path.substring(path.lastIndexOf("/")+1,path.indexOf("."));
+		String msg = new String();
+		if(path.indexOf("login")!=-1||path.indexOf("register")!=-1){
 			return true;
-		else{
-			request.setAttribute("msg","您尚未登录");
-			response.sendRedirect("login.jsp");
-			return false;
 		}
+		if(session.getAttribute("uid")!=null) {
+			int identification = (Integer)session.getAttribute("identification");
+			if(admins.contains(subpath)){
+				if(identification==1) {
+					msg = "用户无法访问管理员页面";
+				}
+				else {
+					return true;
+				}
+			}
+			else{
+				if(identification==0) {
+					msg = "管理员无法访问用户界面";
+				}
+				else {
+					return true;
+				}
+			}
+		}
+		session.invalidate();
+		request.setAttribute("msg",msg);
+		response.sendRedirect("login.jsp");
+		return false;
 	}
-
 }
